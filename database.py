05ca08645baf5634738c4a8139686c94ed5a438f
O-GDev <105860521+O-GDev@users.noreply.github.com>
2023-03-20 15:46:41 +0100

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Gbogo321@localhost/Diabetes_db'
+SQLALCHEMY_DATABASE_URL = 'postgres://ecxswfcwcebesf:a14e3f71007bd9077a6aec2aecbbfb3fcb124c9e473194a48cf413d5cba6505c@ec2-3-93-160-246.compute-1.amazonaws.com:5432/d9prqspvakhrd8
+'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
